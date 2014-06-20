fe439f507b24831c0c462d024efbd28b45cd6452
Stephen Zhou <zhoufwind@hotmail.com>
2014-06-20 15:57:29 +0800

    adding highlight function at fuzzy search mode02

--- py_dictContacts01.py
+++ py_dictContacts01.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+#import PrettyTable
+import itertools
+
 _contacts = {}
 
 with open('contacts.txt') as _f:
@@ -10,29 +13,44 @@ with open('contacts.txt') as _f:
 print _contacts.keys()
 
 while True:
-	_search = raw_input("Search info:").strip()
-	if len(_search) == 0:
+	search = raw_input("Search info:").strip()
+	if len(search) == 0:
 		continue
-	if _contacts.has_key(_search):
-		print _search, _contacts[_search]	
-	else:	# start to search the info fuzzy matching mode
-		_info_counter = 0
-		if len(_search) < 2:
+	if search == 'q':
+		break
+	if _contacts.has_key(search):	# start precise query
+		print search, _contacts[search]	
+	else:							# start fuzzy query
+		info_counter = 0
+		if len(search) < 2:
 			print "No valid info..."
 			continue
-		for _name,_value in _contacts.items():	# first layer loop
-			if _name.count(_search) != 0:	# If name contains search, print user 
+		for name,value in _contacts.items():	# first layer loop
+			if name.count(search) != 0:		# If name contains search, print user 
 				
-				_p = _name.find(_search)# High light search, create index position
-				print _name[:_p] + "\033[32;1m%s\033[0m" % _search + _name[_p + len(_search):] + '\t', '\t'.join(_value)
+				p = name.find(search)		# High light search, create index position
+				print name[:p] + "\033[32;1m%s\033[0m" % search + name[p + len(search):] + '\t', '\t'.join(value)
 
-				_info_counter += 1
+				info_counter += 1
 				continue		# if first loop cound user, STOP second loop, continue first loop!
-			for _i in _value:		# second layer loop
-				if _i.count(_search) != 0:
-					print _name + '\t','\t'.join(_value)
-					_info_counter += 1
-		if _info_counter == 0:
+			for i in value:		# second layer loop
+				if i.count(search) != 0:
+					line = name + '\t' + '\t'.join(value)	# find 'value' contains 'search', generate origin output
+					lineSplit = line.split(search)			# we need highlight 'search', so we split the whole output
+					#print lineSplit
+					lineMeger = []				# create temp sequence
+					for j in lineSplit:			# loop adding each part to the temp sequence
+						#j = j.strip('\n')
+						lineMeger.append(j)		# adding each part to temp sequence
+						lineMeger.append("\033[32;1m%s\033[0m" % search)	# adding highlight part
+					lineMeger.pop()				# removing the highlight at end of the temp sequence
+					#print lineMeger
+					lineFin = "".join(itertools.chain(*lineMeger))			# meger the sequence to a complete string
+					print lineFin
+
+					info_counter += 1
+					break
+		if info_counter == 0:
 			print "No valid record..."
 		else:
-			print "Found %s records..." % _info_counter
+			print "Found %s records..." % info_counter
